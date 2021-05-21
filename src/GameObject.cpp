3c0fc3242a88c9045324b0d1b8e4872228770b52
dtsvetkov13@outlook.com <dtsvetkov13@outlook.com>
2021-05-21 17:34:41 +0300

    Implement intersects()

--- src/GameObject.cpp
+++ src/GameObject.cpp
@@ -24,5 +24,8 @@ void GameObject::setRect(const SDL_Rect& rect)
 
 bool GameObject::intersects(const SDL_Rect& rect)
 {
-	return false;
+	return (this->rect.x < (rect.x + rect.w)
+			&& (this->rect.x + this->rect.w) > rect.x
+			&& this->rect.y < (rect.y + rect.h)
+			&& (this->rect.y + this->rect.h) > rect.y);
 }
