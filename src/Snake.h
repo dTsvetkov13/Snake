8f20785e78db7feb925dd80c633e5674a50c00ce
dtsvetkov13@outlook.com <dtsvetkov13@outlook.com>
2021-05-21 15:55:04 +0300

    Inherits IDrawable

--- src/Snake.h
+++ src/Snake.h
@@ -2,14 +2,16 @@
 
 #include "GameObject.h"
 #include "IMoveable.h"
+#include "IDrawable.h"
 
-class Snake : public GameObject, public IMoveable
+class Snake : public GameObject, public IMoveable, public IDrawable
 {
 public:
 	Snake();
 	Snake(const Position& position);
 
 	void move() override;
+	void draw(SDL_Renderer* ren) override;
 private:
-	
+	 
 };
