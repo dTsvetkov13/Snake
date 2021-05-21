2b14bf3978471c413fd9115ddf25e2a1326f8931
dtsvetkov13@outlook.com <dtsvetkov13@outlook.com>
2021-05-21 15:50:47 +0300

    Add implementation

---
+++ src/IDrawable.cpp
@@ -0,0 +1,68 @@
+#include "IDrawable.h"
+
+IDrawable::IDrawable()
+{
+	m_src.x = m_src.y = 0;
+	m_dest.x = m_dest.y = 0;
+	m_dest.h = m_dest.w = 64;
+}
+
+IDrawable::IDrawable(std::string path, SDL_Renderer* ren) : IDrawable()
+{
+	m_texture = getTexture(path, ren);
+}
+
+IDrawable::~IDrawable()
+{
+	SDL_DestroyTexture(m_texture);
+}
+
+void IDrawable::init(std::string path, SDL_Renderer* ren, unsigned int heightParts, unsigned int widthParts)
+{
+	m_src.x = 0;
+	m_src.y = 0;
+	SDL_Surface* sur = IMG_Load(path.c_str());
+	m_src.h = sur->h;
+	m_src.w = sur->w;
+	//TODO: set m_dest's width and height
+	m_texture = getTexture(path, ren);
+}
+
+SDL_Texture* IDrawable::getTexture(std::string path, SDL_Renderer* ren)
+{
+	SDL_Surface* sur = IMG_Load(path.c_str());
+
+	SDL_Texture* tex = SDL_CreateTextureFromSurface(ren, sur);
+
+	SDL_FreeSurface(sur);
+	return tex;
+}
+
+void IDrawable::setDestRect(SDL_Rect* dest)
+{
+	this->m_dest = *dest;
+}
+
+SDL_Texture* IDrawable::getTexture()
+{
+	return m_texture;
+}
+
+SDL_Rect* IDrawable::getSrcRect()
+{
+	return &m_src;
+}
+SDL_Rect* IDrawable::getDestRect()
+{
+	return &m_dest;
+}
+
+void IDrawable::draw(SDL_Renderer* ren)
+{
+	SDL_RenderCopy(ren, m_texture, &m_src, &m_dest);
+}
+
+void IDrawable::draw(SDL_Renderer* ren, SDL_Rect* dest)
+{
+	SDL_RenderCopy(ren, m_texture, &m_src, dest);
+}
--- src/IDrawable.h
+++ src/IDrawable.h
@@ -1,12 +1,28 @@
 #pragma once
 
 #include "SDL.h"
+#include "SDL_image.h"
+#include <string>
 
 class IDrawable
 {
 public:
-	virtual void draw() = 0;
+	IDrawable();
+	IDrawable(std::string path, SDL_Renderer* ren);
+	~IDrawable();
+
+	void init(std::string path, SDL_Renderer* ren, unsigned int heightParts, unsigned int widthParts);
+	static SDL_Texture* getTexture(std::string path, SDL_Renderer* ren);
+
+	void setDestRect(SDL_Rect* dest);
+
+	SDL_Texture* getTexture();
+	SDL_Rect* getSrcRect();
+	SDL_Rect* getDestRect();
+
+	virtual void draw(SDL_Renderer* ren);
+	virtual void draw(SDL_Renderer* ren, SDL_Rect* dest);
 private:
-	SDL_Texture* texture;
-	SDL_Rect src, dest;
+	SDL_Texture* m_texture;
+	SDL_Rect m_src, m_dest;
 };
