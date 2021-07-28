28e062018ee96f40ca6ccaebc0252fbf0783ada5
dtsvetkov13@outlook.com <dtsvetkov13@outlook.com>
2021-07-28 22:44:56 +0300

    Add basic functions and fields

--- src/Game.h
+++ src/Game.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "SDL.h"
+#include "World.h"
 
 class Game
 {
@@ -9,12 +10,23 @@ public:
 
 	static Game* getInstance();
 
+	void init(const char* title, int x, int y, int width, int height, bool fullscreen);
+	
+	void handleEvent();
+	void update();
+	void render();
+	void clean();
+
+	inline bool running() { return m_isRunning; }
 private:
 	Game();
 	Game(Game const&) = delete;
 	void operator=(Game const&) = delete;
 
-	static Game* instance;
+	static Game* m_instance;
 
-	SDL_Renderer* renderer;
+	World m_world;
+	SDL_Window* m_window;
+	bool m_isRunning = false;
+	SDL_Renderer* m_renderer;
 };
